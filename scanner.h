f6bc2ec5527aa9a19a060ab5537149f8a23aa3b9
KolouchPetr <kolouch.petr4@email.cz>
2022-11-15 21:51:28 +0100

    WIP Reworked get_next_state again
    Also removed "private" functions and data structures from scanner.h. I
    don't know if you guys like it, I guess not so we will just revert it.

--- scanner.h
+++ scanner.h
@@ -3,31 +3,8 @@
 
 #include <stdlib.h>
 #include "token.h"
-
-
-typedef enum {
-/* Every state in the state machine has their own enum...
- * Except: End state that have no arrow coming from them
- */
-    S_START = 0,
-    S_DONE,
-    S_SINGLE_LINE_COMMENT,
-    S_START_OF_COMMENT_OR_DIV,
-    S_BLOCK_COMMENT,
-    S_POSSIBLE_END_OF_BLOCK_COMMENT,
-    S_QUESTION_MARK,
-    S_VARIABLE_ID,
-    S_GREATER_THAN,
-    S_LESSER_THAN,
-    S_IDENTIFIER,
-    S_INT_LITERAL,
-    S_FLOAT_LITERAL_DEC,
-    S_FLOAT_LITERAL_E,
-    S_STRING_LITERAL_DQ,
-    S_STRING_LITERAL_SQ,
-    // PHP_BEGIN,
-    // PHP_END,
-} ScannerState;
+#include "lex.h"
+#include <stdbool.h>
 
 /**
  * Reads the whole token from the input file and returns it as a token
@@ -35,18 +12,4 @@ typedef enum {
  * @return The new token
  */
 Token scan_next_token(FILE* file);
-
-/**
- * Get the next state from the input character and a current state.
- * - When START state is returned, it means that the token is finished and that
- *   the last c isn't a part of it.
- * - When DONE state is returned, it means that the token is finsished and
- *   the last c is a part of it.
- *
- * @param current_state Current state
- * @param c Current character
- * @return New state
- */
-ScannerState get_next_state(ScannerState current_state, char c, Lex* lex_out);
-
 #endif
