9b7855228c3873c1fe402748f5af1841bc94393e
KolouchPetr <kolouch.petr4@email.cz>
2022-11-19 14:26:50 +0100

    added a few test files

---
+++ test-scripts/escape_sequences.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+$escapeBackslash = "Ko\\Lo";
+$escapeTab = "Ko\tLo";
+$escapeNewLine = "Ko\nLo";
+$escapeDollar = "Ko\$Lo";
+$escapeQuote = "Ko\'Lo";
+$escapeHexadecimal = "Ko\xFFLo";
+$escapeOctal = "Ko\377Lo";
+//$escapeHexadecimal = "Ko\x00Lo";
+//$escapeOctal = "Ko\400Lo";
+//$escapeHexadecimalShort = "Ko\xFLo";
+//$escapeOctalShort = "Ko\1Lo";
+?>
---
+++ test-scripts/escape_sequences_error.php
@@ -0,0 +1,4 @@
+<?php declare(strict_types = 1);
+$escapeHexadecimal = "Ko\x00Lo";
+$escapeOctal = "Ko\400Lo";
+?>
