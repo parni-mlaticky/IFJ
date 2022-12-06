1817dd73d1c355d8f16849168367524cb8666249
KolouchPetr <kolouch.petr4@email.cz>
2022-12-06 15:27:15 +0100

    code generator functions commented

--- codeGenerator.h
+++ codeGenerator.h
@@ -5,8 +5,17 @@
 #include "list.h"
 #include "semanticError.h"
 
+/**
+ * @brief generates code for expression
+ * 
+ * @param root root node of the expression
+ * @param isLeftSideOfAssignment left side of assigment
+ * @param localSymtable local symtable
+ * @param globalSymtable global symtable
+ */
 void generateExpressionCode(Nonterminal *root, bool isLeftSideOfAssignment, ht_table_t *localSymtable,
                              ht_table_t* globalSymtable);
+
 void generateToBoolFunction();
 void generateToFloatFunction();
 void generateEnforceTypesFunction();
@@ -29,5 +38,19 @@ void generateNormalizeTypes();
 void generateNullToString();
 void generateRelationTypecast();
 void defineFunctionVars(ht_table_t symtable);
+
+/**
+ * @brief count number of escape sequences in a string
+ * 
+ * @param string string to check
+ * @return int number of sequences
+ */
 int countEscapeSequences(char *string);
+
+/**
+ * @brief converts enym dataType to string
+ * 
+ * @param dType dataType
+ * @return char* converted string
+ */
 char* enumTypeToStr(dataType dType);
