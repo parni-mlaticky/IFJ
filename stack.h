5aeeb120afd36a11db3da1e90b72ffcec8a091cc
KolouchPetr <kolouch.petr4@email.cz>
2022-12-06 15:10:52 +0100

    stack functions commented

--- stack.h
+++ stack.h
@@ -28,19 +28,104 @@ typedef struct stack{
     stackElement* top;
 } stack;
 
+/**
+ * @brief check if stack contains only top terminal
+ * 
+ * @param s stack
+ * @return true if contains only top terminal, false otherwise
+ */
 bool containsOnlyTopNonterm(stack* s);
+
+/**
+ * @brief inits the stack
+ * 
+ * @param s stack to init
+ */
 void stackInit(stack* s);
+
+/**
+ * @brief pushes token into the stack
+ * 
+ * @param s stack to push to
+ * @param t terminal type
+ * @param token token to push
+ */
 void stackPushTerminal(stack* s, terminalType t, Token* token);
+
+/**
+ * @brief pushes function terminal into the stack
+ * 
+ * @param s stack to push to
+ * @param token token to push to
+ * @param args function args
+ */
 void stackPushFuncallTerminal(stack* s, Token* token, nontermList* args);
 
+/**
+ * @brief pops the stack
+ * 
+ * @param s stack to pop
+ */
 void stackPop(stack* s);
-stackElement* stackPeek(stack* s);
-void stackDispose(stack* s);
-bool stackIsEmpty(stack* s);
-stackElement* getTopTerminal(stack* s);
-void stackInsertHandle(stack* s);
-void stackPushNonterminal(stack* s, Nonterminal* nonterm);
-stackElement* findHandle(stack* s);
-void stackMultiPop(stack* s, int count);
-void reduction(stack* s);
 
+/**
+ * @brief returns element at the top of the stack, without popping it
+ * 
+ * @param s stack to peek
+ * @return stackElement* element at the top
+ */
+stackElement* stackPeek(stack* s);
+
+/**
+ * @brief disposes the whole stack
+ * 
+ * @param s stack to dispose
+ */
+void stackDispose(stack* s);
+
+/**
+ * @brief tells if the stack is empty
+ * 
+ * @param s stack
+ * @return true if stack is empty, false otherwise
+ */
+bool stackIsEmpty(stack* s);
+
+/**
+ * @brief gets the terminal at the top of the stack
+ * 
+ * @param s stack to get the item from
+ * @return stackElement* top terminal if found, null otherwise
+ */
+stackElement* getTopTerminal(stack* s);
+
+/**
+ * @brief inserts handle into the stack
+ * 
+ * @param s stack to insert into
+ */
+void stackInsertHandle(stack* s);
+
+/**
+ * @brief pushes a nonterminal into the stack
+ * 
+ * @param s stack to push to
+ * @param nonterm nonterminal to push
+ */
+void stackPushNonterminal(stack* s, Nonterminal* nonterm);
+
+/**
+ * @brief finds handle in the stack
+ * 
+ * @param s stack to search in
+ * @return stackElement* handle if found, null otherwise
+ */
+stackElement* findHandle(stack* s);
+
+/**
+ * @brief pops multiple times from the stack
+ * 
+ * @param s stack to pop
+ * @param count times to pop the stack
+ */
+void stackMultiPop(stack* s, int count);
